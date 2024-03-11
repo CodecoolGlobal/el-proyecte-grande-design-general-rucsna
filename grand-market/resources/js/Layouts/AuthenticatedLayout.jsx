8b7e426b7650ababe81138ef8ecb9d1f5e05a767
Nolex01 <noel.szabo02@icloud.com>
2024-03-11 17:20:05 +0100

    feat: Add 'My Orders' to the Navbar

--- grand-market/resources/js/Layouts/AuthenticatedLayout.jsx
+++ grand-market/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('orders.index')} active={route().current('orders.index')}>
+                                    My Orders
+                                </NavLink>
                             </div>
                         </div>
 

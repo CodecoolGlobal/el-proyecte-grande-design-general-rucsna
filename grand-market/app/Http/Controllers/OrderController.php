d8083848db7449c63873808d9096c7b1fe5bca99
Nolex01 <noel.szabo02@icloud.com>
2024-03-11 16:20:58 +0100

    feat: Create index function in OrderController

--- grand-market/app/Http/Controllers/OrderController.php
+++ grand-market/app/Http/Controllers/OrderController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Inertia\Inertia;
+use Inertia\Response;
+
 class OrderController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $orders = Order::all();
+
+        return Inertia::render('Orders/All', [
+            'orders' => $orders
+        ]);
+    }
 }

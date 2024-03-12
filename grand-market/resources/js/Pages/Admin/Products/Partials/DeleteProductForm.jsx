e5628ec917564b81f64bda8f72089036c8bc7501
Nolex01 <noel.szabo02@icloud.com>
2024-03-12 19:27:23 +0100

    feat: Create delete product form for delete product page

---
+++ grand-market/resources/js/Pages/Admin/Products/Partials/DeleteProductForm.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { useForm } from '@inertiajs/react';
+import InputLabel from '@/Components/InputLabel';
+import TextInput from '@/Components/TextInput';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { Transition } from '@headlessui/react';
+
+const DeleteProductForm = ({ product }) => {
+
+    const { data, setData, delete: destroy, errors, processing, recentlySuccessful } = useForm({
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        stock: product.stock
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        destroy(route('admin.product.edit', product.id));
+    };
+
+    return (
+        <section className="max-w-xL">
+            <header>
+                <h2 className="text-lg font-medium text-gray-900">Product Information</h2>
+            </header>
+
+            <form onSubmit={handleSubmit} className="mt-6 space-y-6">
+                <div>
+                    <InputLabel htmlFor="name" value="Name" />
+
+                    <TextInput
+                        id="name"
+                        className="mt-1 block w-full"
+                        value={data.name}
+                        disabled
+                    />
+                </div>
+
+                <div>
+                    <InputLabel htmlFor="description" value="Description" />
+
+                    <TextInput
+                        id="description"
+                        className="mt-1 block w-full"
+                        value={data.description}
+                        disabled
+                    />
+                </div>
+
+                <div>
+                    <InputLabel htmlFor="price" value="Price" />
+
+                    <TextInput
+                        id="price"
+                        className="mt-1 block w-full"
+                        value={data.price}
+                        disabled
+                    />
+                </div>
+
+                <div>
+                    <InputLabel htmlFor="stock" value="Stock" />
+
+                    <TextInput
+                        id="stock"
+                        className="mt-1 block w-full"
+                        value={data.stock}
+                        disabled
+                    />
+                </div>
+
+                <div className="flex items-center gap-4">
+                    <PrimaryButton disabled={processing}>Confirm Delete</PrimaryButton>
+
+                    <Transition
+                        show={recentlySuccessful}
+                        enter="transition ease-in-out"
+                        enterFrom="opacity-0"
+                        leave="transition ease-in-out"
+                        leaveTo="opacity-0"
+                    >
+                        <p className="text-sm text-gray-600">Deleted.</p>
+                    </Transition>
+                </div>
+            </form>
+            
+        </section>
+    );
+};
+
+export default DeleteProductForm;

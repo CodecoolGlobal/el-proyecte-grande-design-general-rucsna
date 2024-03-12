1bc5f6f60ef7f6d022cbf499d7f42887af9cf109
Nolex01 <noel.szabo02@icloud.com>
2024-03-12 20:17:04 +0100

    feat: Add 'Create New Product' button to admin product list page

--- grand-market/resources/js/Pages/Admin/Products/All.jsx
+++ grand-market/resources/js/Pages/Admin/Products/All.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import NavLink from '@/Components/NavLink';
 
 export default function All({ auth, products }) {
 
@@ -31,6 +32,9 @@ export default function All({ auth, products }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <NavLink href={route('admin.product.create')}>
+                        Create New Product
+                    </NavLink>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             <h3 className="text-lg font-semibold mb-4">Products</h3>

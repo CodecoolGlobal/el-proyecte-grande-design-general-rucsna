c324a6bb44e31fe647733eb974535f66ff6fca53
Nolex01 <noel.szabo02@icloud.com>
2024-03-12 19:35:33 +0100

    refactor: Move 'admin.product.confirm.delete' route to bottom

--- grand-market/routes/web.php
+++ grand-market/routes/web.php
@@ -56,9 +56,9 @@ Route::middleware('admin')->group(function () {
 
     Route::get('/admin/products', [AdminController::class, 'listProducts'])->name('admin.products.list');
     Route::get('/admin/product/{product}', [AdminController::class, 'showProduct'])->name('admin.product.show');
-    Route::get('/admin/product/delete/{product}', [AdminController::class, 'confirmProductDelete'])->name('admin.product.confirm.delete');
     Route::patch('/admin/product/{product}', [AdminController::class, 'editProduct'])->name('admin.product.edit');
     Route::delete('/admin/product/{product}', [AdminController::class, 'deleteProduct'])->name('admin.product.delete');
+    Route::get('/admin/product/delete/{product}', [AdminController::class, 'confirmProductDelete'])->name('admin.product.confirm.delete');
 });
 
 Route::middleware('auth')->group(function () {

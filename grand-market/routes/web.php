64af55c10ded2a96c0cadf40249a2b7210f144c7
Nolex01 <noel.szabo02@icloud.com>
2024-03-13 18:01:10 +0100

    feat: Add 'cart.index' route to web routes

--- grand-market/routes/web.php
+++ grand-market/routes/web.php
@@ -52,6 +52,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
     Route::patch('/new/order', [OrderController::class, 'storeOrder'])->name('order.store');
 
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

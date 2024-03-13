54b9cbb081cce70134329d390cdca471e16bb196
Anna Pomichal <rucsna@gmail.com>
2024-03-13 12:00:05 +0100

    feat: Create new function for limited product list

--- grand-market/app/Http/Controllers/ProductController.php
+++ grand-market/app/Http/Controllers/ProductController.php
@@ -5,25 +5,40 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 use Spatie\FlareClient\Http\Exceptions\NotFound;
 
 class ProductController extends Controller
 {
-    public function getByCategory($categoryName, Request $request)
+    public function limitedProductList(): JsonResponse|Response
+    {
+        $categories = Category::all();
+        if(sizeof($categories) === 0){
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        $limitedProducts = [];
+        foreach ($categories as $category){
+            $products = Product::query()->where('id_category', $category->id)->limit(5)->get();
+            $limitedProducts[$category->name] = $products;
+        }
+
+        return Inertia::render('Welcome', [
+            'limitedProducts' => $limitedProducts
+        ]);
+    }
+
+    public function getByCategory($categoryName)
     {
         $category = Category::query()->where('name', $categoryName)->first();
         if(!$category){
             return response()->json(['error' => 'Category not found'], 404);
         }
 
-        $limit = $request->input('limit');
         $productsQuery = Product::query()->where('id_category', $category->id);
-
-        if($limit !== null){
-            $productsQuery->limit($limit);
-        }
         $products = $productsQuery->get();
 
         return Inertia::render('Products', [

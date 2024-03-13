be7e5d5db1de65fa04b17e88c3bd5d2ded3f33e2
Nolex01 <noel.szabo02@icloud.com>
2024-03-13 18:01:55 +0100

    feat: Create storeOrder function in OrderController

--- grand-market/app/Http/Controllers/OrderController.php
+++ grand-market/app/Http/Controllers/OrderController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Order;
 use Inertia\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -18,4 +20,51 @@ class OrderController extends Controller
             'orders' => $orders
         ]);
     }
+
+    public function storeOrder(Request $request)
+    {
+        $validatedData = $request->validate([
+            'postal_code' => 'required|string',
+            'city' => 'required|string',
+            'address' => 'required|string',
+            'phone_number' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+    
+        $userId = auth()->id();
+    
+        $cartItems = $request->input('cartItems');
+    
+        $totalPrice = 0;
+    
+        $order = new Order();
+        $order->user_id = $userId;
+        $order->postal_code = $validatedData['postal_code'];
+        $order->city = $validatedData['city'];
+        $order->address = $validatedData['address'];
+        $order->phone_number = $validatedData['phone_number'];
+        $order->description = $validatedData['description'];
+    
+        foreach ($cartItems as $item) {
+            $quantity = $item['quantity'] ?? 1;
+            $totalPrice += $item['price'] * $quantity;
+        }
+    
+        $order->total_price = $totalPrice;
+        $order->save();
+    
+        $orderId = $order->id;
+    
+        foreach ($cartItems as $item) {
+            $productId = $item['id'];
+    
+            DB::table('product_orders')->insert([
+                'order_id' => $orderId,
+                'product_id' => $productId,
+            ]);
+        }
+    
+        return redirect()->route('orders.index');
+    }
+    
 }

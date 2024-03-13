20070c02aa85f20cb1156040f2c6399708f8d7ed
Nolex01 <noel.szabo02@icloud.com>
2024-03-13 20:24:45 +0100

    fix: Remove 'cartItems' from storeProduct function

--- grand-market/app/Http/Controllers/AdminController.php
+++ grand-market/app/Http/Controllers/AdminController.php
@@ -130,7 +130,6 @@ class AdminController extends Controller
             'price' => 'required|numeric|min:0',
             'stock' => 'required|integer|min:0',
             'categoryId' => 'required|integer|min:0',
-            'cartItems' => 'required|array',
         ]);
     
         $product = new Product();

47c3c996266589a58c25b2da50a05ab9cbe5425b
Anna Pomichal <rucsna@gmail.com>
2024-03-27 20:05:40 +0100

    feat: Add search option to Welcome.jsx

--- grand-market/resources/js/Pages/Welcome.jsx
+++ grand-market/resources/js/Pages/Welcome.jsx
@@ -3,10 +3,10 @@ import LimitedProducts from "@/Pages/LimitedProducts.jsx";
 import GuestLayout from '@/Layouts/GuestLayout.jsx';
 import ShoppingCartLayout from '@/Layouts/ShoppingCartLayout.jsx';
 import React, { useState } from 'react';
-
 export default function Welcome({ auth }) {
     const { props: { products } } = usePage();
     const [cartItemsCount, setCartItemsCount] = useState(0);
+    const [searchedProducts, setSearchedProducts] = useState([]);
 
     const updateCartItemsCount = (count) => {
         setCartItemsCount(count);
@@ -19,7 +19,7 @@ export default function Welcome({ auth }) {
         setCartItemsCount(cartItems.length);
     };
     return (
-        <GuestLayout>
+        <GuestLayout setSearchedProducts={setSearchedProducts}>
             <ShoppingCartLayout cartItemsCount={cartItemsCount} updateCartItemsCount={updateCartItemsCount} />
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
@@ -49,33 +49,18 @@ export default function Welcome({ auth }) {
                         </>
                     )}
                 </div>
-
-                <div className="max-w-7xl mx-auto p-6 lg:p-8">
-                    <div className="mt-16">
+                {searchedProducts.length > 0 ? (
+                    <LimitedProducts searchedProducts={searchedProducts}/>
+                ) : (
+                    <div className="max-w-7xl mx-auto p-6 lg:p-8">
                         <div className="grid grid-cols-1 gap-6 lg:gap-8">
                             <LimitedProducts category="Clothing"/>
-                            <LimitedProducts category="Household"/>
-                            <LimitedProducts category="Electronics"/>
                             <LimitedProducts category="Books"/>
+                            <LimitedProducts category="Electronics"/>
+                            <LimitedProducts category="Household"/>
                         </div>
-                    </div>
-
-                    <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
-                        <div className="text-center text-sm sm:text-start">&nbsp;</div>
-                    </div>
-                </div>
+                    </div>)}
             </div>
-
-            <style>{`
-                .bg-dots-darker {
-                    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
-                }
-                @media (prefers-color-scheme: dark) {
-                    .dark\\:bg-dots-lighter {
-                        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-                    }
-                }
-            `}</style>
         </GuestLayout>
     );
 }

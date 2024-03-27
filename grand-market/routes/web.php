edf7177fee3c74eb0aebc38f169f0896d25356df
Nolex01 <noel.szabo02@icloud.com>
2024-03-27 14:22:24 +0100

    feat: Add admin.order.confirm.delete route to web routes

--- grand-market/routes/web.php
+++ grand-market/routes/web.php
@@ -30,6 +30,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('admin')->group(function () {
     Route::get('/admin/orders', [AdminController::class, 'listOrders'])->name('admin.orders.list');
+    Route::get('/admin/order/delete/{order}', [AdminController::class, 'confirmOrderDelete'])->name('admin.order.confirm.delete');
 
     Route::get('/admin/users', [AdminController::class, 'listUsers'])->name('admin.users.list');
 
